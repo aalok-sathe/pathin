827df78429f412e156ea598e51a30eff24aeca7d
aalok-sathe <aalok.sathe@gmail.com>
2020-05-11 11:12:23 +0530

    make script installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='pathin',
+      version='0.1',
+      description='Utility to conveniently add to PATH',
+      author='Aalok Sathe',
+      author_email='aalok-sathe@acm.org',
+      url='https://github.com/aalok-sathe/pathin.git',
+      scripts=['pathin'])
